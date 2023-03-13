48c805dd56bcc77804e790a36fff9075b2d43b12
Robin Schatzl <tmai02476@gmail.com>
2023-03-13 15:04:56 +0100

    N/A

--- src/main/java/com/example/spring/GreetingsController.java
+++ src/main/java/com/example/spring/GreetingsController.java
@@ -1,28 +1,36 @@
 package com.example.spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greetings")
 public class GreetingsController {
 
+    String name = "";
+
     @GetMapping("/de")
     public String getGreetingsDe(){
-        return "Hallo";
+        return "Hallo ";
     }
 
     @GetMapping("/en")
     public String getGreetingsEn() {
-        return "Hello";
+        return "Hello ";
     }
 
     @GetMapping("/{name}")
-    public String getGreetings(@PathVariable String name) {
-        return "Hallo " + name;
+    public String getGreetings(@PathVariable String name, @RequestParam String greeting) {
+        return greeting + " " + name;
     }
+
+    @PostMapping
+    public void addName(@RequestBody String newName) {
+        name = newName;
+
+    }
+
+
+
 
 }
 
